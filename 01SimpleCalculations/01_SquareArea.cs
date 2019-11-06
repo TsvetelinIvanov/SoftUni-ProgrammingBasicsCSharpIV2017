45dc33f97712a34b645a0fee4fbde2d0f9713ac9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-06 14:59:29 +0200

    Update 01_SquareArea.cs

--- 01SimpleCalculations/01_SquareArea.cs
+++ 01SimpleCalculations/01_SquareArea.cs
@@ -11,7 +11,7 @@ namespace _01_SquareArea
         static void Main(string[] args)
         {
             Console.Write("a = ");
-            var a = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
             var area = a * a;
             Console.Write("Square = ");
             Console.WriteLine(area);
