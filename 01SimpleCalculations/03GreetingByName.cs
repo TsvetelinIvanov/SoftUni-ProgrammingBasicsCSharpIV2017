e0c235fcad7422832d0e98153310f8176d6a7473
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-06 14:48:01 +0200

    Create 03GreetingByName.cs

---
+++ 01SimpleCalculations/03GreetingByName.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03GreetingByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter yor name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
