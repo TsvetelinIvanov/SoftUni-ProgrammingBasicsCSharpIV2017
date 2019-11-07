11bb7a97902a30067a74147a70a5c588f3596ee0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-07 20:19:15 +0200

    Create 07SumSeconds.cs

---
+++ 02SimpleConditions/07SumSeconds.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time1 = int.Parse(Console.ReadLine());
+            int time2 = int.Parse(Console.ReadLine());
+            int time3 = int.Parse(Console.ReadLine());
+
+            int sumTime = time1 + time2 + time3;
+            int minutes = sumTime / 60;
+            int seconds = sumTime % 60;
+
+            Console.WriteLine(minutes + ":" + "{0:00}", seconds);
+        }     
+    }
+}
