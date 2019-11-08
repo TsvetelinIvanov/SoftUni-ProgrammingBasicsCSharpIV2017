df2490ed21adf6c51fe6d8c2061a958488ab61d1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 18:18:17 +0200

    Create 11Increasing4Numbers.cs

---
+++ 07SampleProgrammingBasicsExam/11Increasing4Numbers.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11Increasing4Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if ((b - a) < 3) Console.WriteLine("No");
+
+            for (int n1 = a; n1 <= b - 3; n1++)
+            {
+                for (int n2 = a + 1; n2 <= b - 2; n2++)
+                {
+                    for (int n3 = a + 2; n3 <= b - 1; n3++)
+                    {                        
+                        for (int n4 = a + 3; n4 <= b; n4++)
+                        {
+                            if (a <= n1 && n1 < n2 && n2 < n3 && n3 < n4 && n4 <= b)
+                            {
+                                Console.WriteLine("{0} {1} {2} {3} ", n1, n2, n3, n4);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
