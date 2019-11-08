91b5980af0ffc8d67f2e9f49f747b630e881474f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 17:28:10 +0200

    Create 20Digits.cs

---
+++ 06AdvancedLoops/20Digits.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int abc = int.Parse(Console.ReadLine());
+            int a = abc / 100;
+            int b = (abc / 10) % 10;
+            int c = abc % 10;
+            int number = abc;
+            for (int row = 0; row < a + b; row++)
+            {
+                for (int column = 0; column < a + c; column++)
+                {
+                    if (number % 5 == 0)
+                    {
+                        number -= a;
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        number -= b;
+                    }
+                    else
+                    {
+                        number += c;
+                    }
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
