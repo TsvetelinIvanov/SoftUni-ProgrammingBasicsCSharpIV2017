fc9a7de747d13d79a06db3160288d17793c363c8
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 17:36:14 +0200

    Create 01TriangleAreaInPlane.cs

---
+++ 07SampleProgrammingBasicsExam/01TriangleAreaInPlane.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01TriangleAreaInPlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+            double a = Math.Abs(x2 - x3);
+            double h = Math.Abs(y1 - y2);
+            double S = a * h / 2.0;
+            Console.WriteLine(S);
+        }
+    }
+}
