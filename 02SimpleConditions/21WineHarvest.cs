124977a8a10ca0837791a6bcf1e5f1b048ca33cc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 08:21:09 +0200

    Create 21WineHarvest.cs

---
+++ 02SimpleConditions/21WineHarvest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21WineHarvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine());
+            int Z = int.Parse(Console.ReadLine());
+            int W = int.Parse(Console.ReadLine());
+            var grapes = X * Y;
+            var wine = (0.4 * grapes) / 2.5;
+            var differenceWine = Math.Abs(wine - Z);
+            if (wine < Z)
+            {
+                Console.WriteLine("It will be a tough winter! More {0} liters wine needed.", Math.Floor(differenceWine));
+            }
+            else
+            {
+                var winePerW = differenceWine / W;
+                Console.WriteLine("Good harvest this year! Total wine: {0} liters.", Math.Floor(wine));
+                Console.WriteLine("{0} liters left -> {1} liters per person.",
+                    Math.Ceiling(differenceWine), Math.Ceiling(winePerW));
+            }
+        }
+    }
+}
