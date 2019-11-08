b3ad2925994c04a2c8004922f3a826dac6d65c23
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 17:25:05 +0200

    Create 19StopNumber.cs

---
+++ 06AdvancedLoops/19StopNumber.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19StopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int S = int.Parse(Console.ReadLine());
+            for (int i = M; i >= N; i--)
+            {
+                if(i % 6 == 0)
+                {
+                    if (i == S) break;
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+    }
+}
