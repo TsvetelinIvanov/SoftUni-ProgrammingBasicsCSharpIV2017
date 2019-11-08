8bcb1adeeee31e70c80430934243760f6df2b8ac
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 12:00:51 +0200

    Create 01RectangleOf10x10Stars.cs

---
+++ 05DrawWithLoops/01RectangleOf10x10Stars.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01RectangleOf10x10Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string star = new string('*', 10);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(star);
+            }
+        }
+    }
+}
