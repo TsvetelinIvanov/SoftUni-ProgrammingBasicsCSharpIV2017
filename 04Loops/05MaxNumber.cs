06f85130293c1262c74134b0b45b76f8b10d89f3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 10:58:01 +0200

    Create 05MaxNumber.cs

---
+++ 04Loops/05MaxNumber.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                int curentNumber = int.Parse(Console.ReadLine());
+                if (curentNumber > max)
+                {
+                    max = curentNumber;
+                }                
+            }
+            Console.WriteLine("max = " + max);
+        }
+    }
+}
