3460b6c7ca102332f0f474956eca7d0b6fc01c1e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 20:47:55 +0200

    Create 06NumberGenerator.cs

---
+++ ExamProgrammingBasics7V2017/06NumberGenerator.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06NumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int L = int.Parse(Console.ReadLine());
+            int spetialNumber = int.Parse(Console.ReadLine());
+            int controlNumber = int.Parse(Console.ReadLine());
+           
+            for (int m = M; m >= 1; m--)
+            {
+                for (int n = N; n >= 1; n--)
+                {
+                    for (int l = L; l >= 1; l--)
+                    {
+                        int mnl = (m * 100) + (n * 10) + l;
+                        if (mnl % 3 == 0)
+                        {
+                            spetialNumber += 5;
+                        }
+                        else if (mnl % 10 == 5)
+                        {
+                            spetialNumber -= 2;
+                        }
+                        else if (mnl % 2 == 0)
+                        {
+                            spetialNumber *= 2;
+                        }
+                        if (spetialNumber >= controlNumber) break;
+                    }
+                    if (spetialNumber >= controlNumber) break;
+                }
+                if (spetialNumber >= controlNumber) break;
+            }
+
+            if (spetialNumber >= controlNumber)
+            {
+                Console.WriteLine("Yes! Control number was reached! Current special number is {0}.", spetialNumber);
+            }
+            else
+            {
+                Console.WriteLine("No! {0} is the last reached special number.", spetialNumber);
+            }
+        }
+    }
+}
