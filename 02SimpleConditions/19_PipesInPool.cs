967477ac6d3e67416296ec5cd14ba8f660d8d631
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 08:13:00 +0200

    Create 19_PipesInPool.cs

---
+++ 02SimpleConditions/19_PipesInPool.cs
@@ -0,0 +1,27 @@
+using System;
+
+class _19_PipesInPool
+{
+    static void Main(string[] args)
+    {
+        double V = double.Parse(Console.ReadLine());
+        double P1 = double.Parse(Console.ReadLine());
+        double P2 = double.Parse(Console.ReadLine());
+        double H = double.Parse(Console.ReadLine());
+        double waterP1 = P1 * H;
+        double waterP2 = P2 * H;
+        double water = waterP1 + waterP2;
+        if (V >= water)
+        {
+            double total = Math.Truncate(water / (V / 100));
+            double Pp1 = Math.Truncate(waterP1 / (water / 100));
+            double Pp2 = Math.Truncate(waterP2 / (water / 100));
+            Console.WriteLine("The pool is {0}% full. Pipe 1: {1}%. Pipe 2: {2}%.", total, Pp1, Pp2);
+        }
+        else
+        {
+            double total = water - V;
+            Console.WriteLine("For {0} hours the pool overflows with {1:f1} liters.", H, total);
+        }
+    }
+}
