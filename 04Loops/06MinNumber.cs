90081f79227eca2943b15be6e45253cf2e3cfa68
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 11:01:17 +0200

    Create 06MinNumber.cs

---
+++ 04Loops/06MinNumber.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber < min) min = currentNumber;                
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
