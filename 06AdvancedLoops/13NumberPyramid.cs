8fbb2111d9048eafa979c85c40764c782751c887
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 16:54:32 +0200

    Create 13NumberPyramid.cs

---
+++ 06AdvancedLoops/13NumberPyramid.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 1;
+            for (int row = 1; row <= n; row++)
+            {
+                for (int column = 1; column <= row; column++)
+                {
+                    if (number > n) break;
+                    Console.Write("{0} ", number);
+                    number++;                    
+                }                
+                Console.WriteLine();
+                if (number > n) break; //for removing the spaces.
+            }
+        }
+    }
+}
