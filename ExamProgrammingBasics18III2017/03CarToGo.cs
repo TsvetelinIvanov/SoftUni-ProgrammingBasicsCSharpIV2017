c86dc708c973f4dcb2185ad62394f0d0e6dd3d42
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 20:59:40 +0200

    Create 03CarToGo.cs

---
+++ ExamProgrammingBasics18III2017/03CarToGo.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03CarToGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double price = 0.0;
+            string carType = string.Empty;
+            string klass = string.Empty;
+
+            if (budget <= 100)
+            {
+                klass = "Economy class";
+                if (season == "Summer")
+                {
+                    carType = "Cabrio"; price = budget * 0.35;                    
+                }
+                else
+                {
+                    carType = "Jeep"; price = budget * 0.65;
+                }
+            }
+            else if (budget <= 500)
+            {
+                klass = "Compact class";
+                if (season == "Summer")
+                {
+                    carType = "Cabrio"; price = budget * 0.45;
+                }
+                else
+                {
+                    carType = "Jeep"; price = budget * 0.80;
+                }
+            }
+            else
+            {
+                klass = "Luxury class";
+                carType = "Jeep"; price = budget * 0.9;                
+            }
+
+            Console.WriteLine(klass);
+            Console.WriteLine("{0} - {1:f2}", carType, price);
+        }
+    }
+}
