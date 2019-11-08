76240d63ddae7656455c16adb467d8cb574c720d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 21:36:15 +0200

    Create 05ChristmasHat.cs

---
+++ ExamProgrammingBasics18XII2016/05ChristmasHat.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05ChristmasHat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = 4 * n + 1;
+
+            Console.WriteLine(@"{0}/|\{0}", new string('.', 2 * n - 1));
+
+            Console.WriteLine(@"{0}\|/{0}", new string('.', 2 * n - 1));
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                Console.WriteLine("{0}*{1}*{1}*{0}", new string('.', 2 * n - 1 - i), new string('-', i));
+            }
+
+            Console.WriteLine(new string('*', width));
+
+            for (int i = 0; i < n * 2; i++)
+            {
+                Console.Write("*.");
+            }
+            Console.WriteLine("*");
+
+            Console.WriteLine(new string('*', width));
+        }
+    }
+}
