4d94026f8f876abd3219f6a886e785c785fed6ba
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 16:57:11 +0200

    Create 14NumberTable.cs

---
+++ 06AdvancedLoops/14NumberTable.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14NumberTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int column = 0; column < n; column++)
+                {
+                    number = row + column + 1;
+                    if (number >= n)
+                    {
+                        number = 2 * n - number;
+                    }
+                    Console.Write("{0} ", number);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
