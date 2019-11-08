6f152b17a7e66894e6f0273950b83129ed73962c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 16:35:37 +0200

    Create 08Factorial.cs

---
+++ 06AdvancedLoops/08Factorial.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            do
+            {
+                factorial = factorial * n;
+                n--;
+            } while (n > 1);
+            
+            Console.WriteLine(factorial);
+        }
+    }
+}
