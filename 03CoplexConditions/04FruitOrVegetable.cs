0b9a0607e1fb1b20ce5e304b2cb468cdf705c2e3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 08:56:42 +0200

    Create 04FruitOrVegetable.cs

---
+++ 03CoplexConditions/04FruitOrVegetable.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thing = Console.ReadLine();
+            if (thing == "banana" || thing == "apple" || thing == "kiwi" || thing == "cherry" ||
+                thing == "lemon" || thing == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if(thing == "tomato" || thing == "cucumber" || thing == "pepper" || thing == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
