7b5bb52e9ecf9918a290e8b674a9d9a4f5f11c40
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 16:47:32 +0200

    Create 11EnterEvenNumber.cs

---
+++ 06AdvancedLoops/11EnterEvenNumber.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11EnterEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter even number: ");
+                    n = int.Parse(Console.ReadLine());
+                    if (n % 2 == 0) break;
+                    Console.WriteLine("The number is not even.");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+            }
+
+            Console.WriteLine("Even number entered: " + n);
+        }
+    }
+}
