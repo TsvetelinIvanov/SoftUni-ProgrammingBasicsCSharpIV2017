c0eb7603998ad48fdbfe468d73490095a86f7391
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 14:46:37 +0200

    Create 13Butterfly.cs

---
+++ 05DrawWithLoops/13Butterfly.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Butterfly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n - 1; i++)
+			      {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(@"{0}\ /{0}", new string('-', n - 2));
+                }
+                else
+                {
+                    Console.WriteLine(@"{0}\ /{0}", new string('*', n - 2));
+                }
+			      }
+            
+            Console.WriteLine("{0}@{0}", new string(' ', n - 1));
+            
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(@"{0}/ \{0}", new string('-', n - 2));
+                }
+                else
+                {
+                    Console.WriteLine(@"{0}/ \{0}", new string('*', n - 2));
+                }
+            }            
+        }
+    }
+}
