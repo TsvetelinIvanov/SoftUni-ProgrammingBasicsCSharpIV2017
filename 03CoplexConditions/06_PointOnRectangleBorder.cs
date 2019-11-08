2ae4262271d7cff9acaa525e18edc79525e0b308
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 09:04:46 +0200

    Create 06_PointOnRectangleBorder.cs

---
+++ 03CoplexConditions/06_PointOnRectangleBorder.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_PointOnRectangleBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            bool checkX = (x == x1 || x == x2) && (y >= y1 && y <= y2);
+            bool checkY = (y == y1 || y == y2) && (x >= x1 && x <= x2);
+
+            if (checkX || checkY)
+            {
+                Console.WriteLine("Border");
+            }
+            else
+            {
+                Console.WriteLine("Inside / Outside");
+            }
+        }
+    }
+}
