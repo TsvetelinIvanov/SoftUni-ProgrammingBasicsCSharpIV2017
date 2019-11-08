7b377095fa9fc818e3902b200b56dfed9e8028b1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-08 19:48:08 +0200

    Create 04Hospital.cs

---
+++ 08ExamPreparationProgrammingBasics18VI2017/04Hospital.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treatedPatiens = 0;
+            int untreatedPatiens = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (treatedPatiens < untreatedPatiens)
+                    {
+                        doctors++;
+                    }
+                }
+                int patiens = int.Parse(Console.ReadLine());
+                if (patiens >= doctors)
+                {
+                    treatedPatiens += doctors;
+                    untreatedPatiens += patiens - doctors;
+                }
+                else
+                {
+                    treatedPatiens += patiens;
+                }
+            }
+
+            Console.WriteLine("Treated patients: {0}.", treatedPatiens);
+            Console.WriteLine("Untreated patients: {0}.", untreatedPatiens);
+        }
+    }
+}
