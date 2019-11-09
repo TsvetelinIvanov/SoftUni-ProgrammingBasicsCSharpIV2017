e3699be1f5ac66ff7409aee620bcd5a1c5708f89
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-09 12:09:32 +0200

    Create 06MaxCombination.cs

---
+++ ExamProgrammingBasicsEvening20XI2016/06MaxCombination.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06MaxCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int maxCombination = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    Console.Write("<{0}-{1}>", i, j);
+                    counter++;
+                    if (counter >= maxCombination) break;
+                }
+                if (counter >= maxCombination) break;
+            }
+        }
+    }
+}
