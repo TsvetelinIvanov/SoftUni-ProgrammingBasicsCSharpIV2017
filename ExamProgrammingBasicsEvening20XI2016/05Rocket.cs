0dff75c218b34b20604246bee9acf55237b1a21a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2019-11-09 12:07:22 +0200

    Create 05Rocket.cs

---
+++ ExamProgrammingBasicsEvening20XI2016/05Rocket.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Rocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = 3 * n;
+            int mid = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(@"{0}/{1}\{0}", new string('.', n + (n / 2) - i), new string(' ', mid));
+                mid += 2;
+            }
+            Console.WriteLine("{0}{1}{0}", new string('.', n / 2), new string('*', n * 2));
+            for (int i = 0; i < n * 2; i++)
+            {
+                Console.WriteLine(@"{0}|{1}|{0}", new string('.', n / 2), new string('\\', n * 2 - 2));
+            }
+            for (int i = 0; i < n / 2; i++)
+            {
+                Console.WriteLine(@"{0}/{1}\{0}", new string('.', (n / 2) - i), new string('*', mid - 2));
+                mid += 2;
+            }
+        }
+    }
+}
