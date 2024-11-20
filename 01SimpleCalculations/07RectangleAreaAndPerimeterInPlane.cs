30dbb65b9eab5e3cf12d3f54cf3bb6007878f42f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-20 16:27:28 +0200

    Update and rename 07RectangleAriaAndPerimeterInPlane.cs to 07RectangleAreaAndPerimeterInPlane.cs

---
+++ 01SimpleCalculations/07RectangleAreaAndPerimeterInPlane.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07RectangleAreaAndPerimeterInPlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            
+            double area = Math.Abs(x1 - x2) * Math.Abs(y1 - y2);
+            double perimeter = 2 * (Math.Abs(x1 - x2) + 2 * Math.Abs(y1 - y2));
+            
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
