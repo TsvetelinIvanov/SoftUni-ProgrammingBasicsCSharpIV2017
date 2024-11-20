6b68788363f5c42028b5db749f59e6fd36f58045
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-20 16:16:37 +0200

    Update 05TrapezoidArea.cs

--- 01SimpleCalculations/05TrapezoidArea.cs
+++ 01SimpleCalculations/05TrapezoidArea.cs
@@ -10,10 +10,11 @@ namespace _05TrapezoidArea
     {
         static void Main(string[] args)
         {
-            var b1 = double.Parse(Console.ReadLine());
-            var b2 = double.Parse(Console.ReadLine());
-            var h = double.Parse(Console.ReadLine());
-            var area = (b1 + b2) * h / 2;
+            double b1 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            
+            double area = (b1 + b2) * h / 2;
             Console.WriteLine("Trapezoid aria = " + area);
         }
     }
