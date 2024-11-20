9d9afd4a08e6a719d88540b768229a5043ce3216
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-20 16:09:42 +0200

    Update 02InchesToCentimeters.cs

--- 01SimpleCalculations/02InchesToCentimeters.cs
+++ 01SimpleCalculations/02InchesToCentimeters.cs
@@ -11,8 +11,9 @@ namespace _02InchesToCentimeters
         static void Main(string[] args)
         {
             Console.Write("inches = ");
-            var inches = double.Parse(Console.ReadLine());
-            var centimeters = inches * 2.54;
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            
             Console.Write("Centimeters = ");
             Console.WriteLine(centimeters);
         }
