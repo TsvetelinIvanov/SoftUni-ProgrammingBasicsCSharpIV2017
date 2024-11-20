45e49c56ab6d6a42731841dc3cd06800ca3a2667
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-20 18:18:05 +0200

    Update 13Days1000OnEarth.cs

--- 01SimpleCalculations/13Days1000OnEarth.cs
+++ 01SimpleCalculations/13Days1000OnEarth.cs
@@ -10,8 +10,8 @@ namespace _13Days1000OnEarth
     {
         static void Main(string[] args)
         {
-            string entry = Console.ReadLine();
-            DateTime date = DateTime.ParseExact(entry, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            string dateString = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dateString, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
             date = date.AddDays(999);
             Console.WriteLine(date.ToString("dd-MM-yyyy"));
         }
