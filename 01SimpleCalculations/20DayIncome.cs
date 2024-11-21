b553a5d08e62522e749daf58c95656a72ca42767
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-22 01:15:30 +0200

    Update 20DayIncome.cs

--- 01SimpleCalculations/20DayIncome.cs
+++ 01SimpleCalculations/20DayIncome.cs
@@ -10,15 +10,15 @@ namespace _20DayIncome
     {
         static void Main(string[] args)
         {
-            var workday = int.Parse(Console.ReadLine());
-            var moneyOnWorkday = double.Parse(Console.ReadLine());
-            var USDtoBGN = double.Parse(Console.ReadLine());
+            int workdaysCount = int.Parse(Console.ReadLine());
+            double moneyPerWorkday = double.Parse(Console.ReadLine());
+            double USDtoBGN = double.Parse(Console.ReadLine());
 
-            var monthIncome = workday * moneyOnWorkday;
-            var yearIncome = (monthIncome * 12) + (monthIncome * 2.5);
-            var tax = (yearIncome * 25) / 100;
-            var disposableYearIncome = (yearIncome - tax) * USDtoBGN;
-            var disposableDayIncome = disposableYearIncome / 365;
+            double monthIncome = workdaysCount * moneyPerWorkday;
+            double yearIncome = (monthIncome * 12) + (monthIncome * 2.5);
+            double tax = (yearIncome * 25) / 100;
+            double disposableYearIncome = (yearIncome - tax) * USDtoBGN;
+            double disposableDayIncome = disposableYearIncome / 365;
 
             Console.WriteLine(Math.Round(disposableDayIncome, 2));
         }
