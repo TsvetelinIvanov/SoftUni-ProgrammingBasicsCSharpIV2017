d2c2d87a0fce2f427175d6cf3d9b0532dfb97187
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-23 19:47:13 +0200

    Update 13AreaOfFigures.cs

--- 02SimpleConditions/13AreaOfFigures.cs
+++ 02SimpleConditions/13AreaOfFigures.cs
@@ -10,30 +10,31 @@ namespace _13AreaOfFigures
     {
         static void Main(string[] args)
         {
-            var figure = Console.ReadLine();
+            string figure = Console.ReadLine();
             double area = 0.0;
             if (figure == "square")
             {
-                var a = double.Parse(Console.ReadLine());
+                double a = double.Parse(Console.ReadLine());
                 area = a * a;
             }
             else if (figure == "rectangle")
             {
-                var a = double.Parse(Console.ReadLine());
-                var b = double.Parse(Console.ReadLine());
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
                 area = a * b;
             }
             else if (figure == "circle")
             {
-                var a = double.Parse(Console.ReadLine());
-                area = Math.PI * a * a;
+                double r = double.Parse(Console.ReadLine());
+                area = Math.PI * r * r;
             }
             else if (figure == "triangle")
             {
-                var a = double.Parse(Console.ReadLine());
-                var b = double.Parse(Console.ReadLine());
-                area = a * b / 2;
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                area = a * h / 2;
             }
+            
             Console.WriteLine(Math.Round(area, 3));
         }
     }
