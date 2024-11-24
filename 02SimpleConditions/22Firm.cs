89c895cb05382cb4194ffced6ef45519e9a4d1f6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-24 02:46:53 +0200

    Update 22Firm.cs

--- 02SimpleConditions/22Firm.cs
+++ 02SimpleConditions/22Firm.cs
@@ -10,13 +10,14 @@ namespace _22Firm
     {
         static void Main(string[] args)
         {
-            var hours = int.Parse(Console.ReadLine());
-            var days = int.Parse(Console.ReadLine());
-            var workers = int.Parse(Console.ReadLine());
-            var workHours = (days - (0.1 * days)) * 8;
-            var overHours = workers * (2 * days);
-            var totalHours = workHours + overHours;
-            var differenceHours = Math.Abs(totalHours - hours);
+            int hours = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            
+            double workHours = (days - (0.1 * days)) * 8;
+            int overHours = workers * (2 * days);
+            double totalHours = workHours + overHours;
+            double differenceHours = Math.Abs(totalHours - hours);
             if (totalHours >= hours)
             {
                 Console.WriteLine("Yes!{0} hours left.", Math.Floor(differenceHours));
