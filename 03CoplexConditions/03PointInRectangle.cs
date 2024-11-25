57d409628c37999da16d5d148cd4860a6d518880
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-26 00:12:45 +0200

    Update 03PointInRectangle.cs

--- 03CoplexConditions/03PointInRectangle.cs
+++ 03CoplexConditions/03PointInRectangle.cs
@@ -10,12 +10,12 @@ namespace _03PointInRectangle
     {
         static void Main(string[] args)
         {
-            var x1 = double.Parse(Console.ReadLine());
-            var y1 = double.Parse(Console.ReadLine());
-            var x2 = double.Parse(Console.ReadLine());
-            var y2 = double.Parse(Console.ReadLine());
-            var x = double.Parse(Console.ReadLine());
-            var y = double.Parse(Console.ReadLine());
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
             if ((x >= x1) && (x <= x2) && (y >= y1) && (y <= y2))
             {
                 Console.WriteLine("Inside");
