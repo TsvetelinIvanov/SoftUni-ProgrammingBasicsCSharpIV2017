732b12fc68b080e2c51025d75164e4da4aef8980
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-26 00:19:18 +0200

    Update 06_PointOnRectangleBorder.cs

--- 03CoplexConditions/06_PointOnRectangleBorder.cs
+++ 03CoplexConditions/06_PointOnRectangleBorder.cs
@@ -16,9 +16,9 @@ namespace _06_PointOnRectangleBorder
             double y2 = double.Parse(Console.ReadLine());
             double x = double.Parse(Console.ReadLine());
             double y = double.Parse(Console.ReadLine());
+            
             bool checkX = (x == x1 || x == x2) && (y >= y1 && y <= y2);
             bool checkY = (y == y1 || y == y2) && (x >= x1 && x <= x2);
-
             if (checkX || checkY)
             {
                 Console.WriteLine("Border");
