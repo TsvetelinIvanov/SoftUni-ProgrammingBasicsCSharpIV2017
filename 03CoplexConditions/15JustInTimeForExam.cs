852fc7cc55a9757a5b1c8bb1c2c4659aa49e82e1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-26 23:31:41 +0200

    Update 15JustInTimeForExam.cs

--- 03CoplexConditions/15JustInTimeForExam.cs
+++ 03CoplexConditions/15JustInTimeForExam.cs
@@ -15,28 +15,50 @@ namespace _15JustInTimeForExam
             int arrivalHour = int.Parse(Console.ReadLine());
             int arrivalMinute = int.Parse(Console.ReadLine());
 
-            int examTime = examHour * 60 + examMinute;
-            int arrivalTime = arrivalHour * 60 + arrivalMinute;
-            int differenceTime = arrivalTime - examTime;
-
-            if (differenceTime < -30) Console.WriteLine("Early");
-            else if (differenceTime <= 0) Console.WriteLine("On time");
-            else Console.WriteLine("Late");
+            int examTimeInMinutes = examHour * 60 + examMinute;
+            int arrivalTimeInMinutes = arrivalHour * 60 + arrivalMinute;
+            int differenceTime = arrivalTimeInMinutes - examTimeInMinutes;
+            if (differenceTime < -30)
+            {
+                Console.WriteLine("Early");
+            }
+            else if (differenceTime <= 0)
+            {
+                Console.WriteLine("On time");
+            }
+            else
+            {
+                Console.WriteLine("Late");
+            }
 
             if (differenceTime != 0)
             {
                 int hour = Math.Abs(differenceTime / 60);
                 int minute = Math.Abs(differenceTime % 60);
-
                 if (hour > 0)
                 {
-                    if (minute < 10) Console.Write(hour + ":0" + minute + " hours");
-                    else Console.Write(hour + ":" + minute + " hours");
+                    if (minute < 10)
+                    {
+                        Console.Write(hour + ":0" + minute + " hours");
+                    }
+                    else
+                    {
+                        Console.Write(hour + ":" + minute + " hours");
+                    }
                 }
-                else Console.Write(minute + " minutes");
+                else
+                {
+                    Console.Write(minute + " minutes");
+                }
 
-                if (differenceTime < 0) Console.WriteLine(" before the start");
-                else Console.WriteLine(" after the start");
+                if (differenceTime < 0)
+                {
+                    Console.WriteLine(" before the start");
+                }
+                else
+                {
+                    Console.WriteLine(" after the start");
+                }
             }
         }
     }
