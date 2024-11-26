9b0352f67d6d3c06c65ea3aa3a64f7761257fb07
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-26 23:03:56 +0200

    Update 10AnimalType.cs

--- 03CoplexConditions/10AnimalType.cs
+++ 03CoplexConditions/10AnimalType.cs
@@ -10,14 +10,20 @@ namespace _10AnimalType
     {
         static void Main(string[] args)
         {
-            var animal = Console.ReadLine();
+            string animal = Console.ReadLine();
             switch (animal)
             {
-                case "dog": Console.WriteLine("mammal"); break;
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
                 case "crocodile":
                 case "tortoise":
-                case "snake": Console.WriteLine("reptile"); break;
-                default: Console.WriteLine("unknown"); break;
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
             }
         }
     }
