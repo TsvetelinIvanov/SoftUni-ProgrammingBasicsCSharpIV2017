8278bdb22c007aa942db361e0f925aec770eabec
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-27 20:32:30 +0200

    Update 18MathTickets.cs

--- 03CoplexConditions/18MathTickets.cs
+++ 03CoplexConditions/18MathTickets.cs
@@ -11,39 +11,73 @@ namespace _18MathTickets
         static void Main(string[] args)
         {
             double budget = double.Parse(Console.ReadLine());
-            var category = Console.ReadLine();
-            int people = int.Parse(Console.ReadLine());
-            var ticketCategory = 0.0;
-            var transport = 0.0;
-            var ticketMoney = 0.0;
-            var ticketPeople = 0.0;
-            var money = 0.0;
+            string category = Console.ReadLine();
+            int peopleCount = int.Parse(Console.ReadLine());
+            
+            double ticketCategory = 0.0;
+            double transportCost = 0.0;
+            double moneyForTickets = 0.0;
+            double ticketsCost = 0.0;
+            double money = 0.0;
+            
             if (category == "VIP")
             {
                 ticketCategory = 499.99;
-                if (people >= 1 && people <= 4) { transport = budget * 0.75; }
-                else if (people >= 5 && people <= 9) { transport = budget * 0.60; }
-                else if (people >= 10 && people <= 24) { transport = budget * 0.50; }
-                else if (people >= 25 && people <= 49) { transport = budget * 0.40; }
-                else if (people >= 50) { transport = budget * 0.25; }
-                ticketMoney = budget - transport;
-                ticketPeople = people * ticketCategory;
-                money = Math.Abs(ticketMoney - ticketPeople);
+                if (peopleCount >= 1 && peopleCount <= 4)
+                {
+                    transportCost = budget * 0.75;
+                }
+                else if (peopleCount >= 5 && peopleCount <= 9)
+                {
+                    transportCost = budget * 0.60;
+                }
+                else if (peopleCount >= 10 && peopleCount <= 24)
+                {
+                    transportCost = budget * 0.50;
+                }
+                else if (peopleCount >= 25 && peopleCount <= 49)
+                {
+                    transportCost = budget * 0.40;
+                }
+                else if (peopleCount >= 50)
+                {
+                    transportCost = budget * 0.25;
+                }
+                
+                moneyForTickets = budget - transportCost;
+                ticketsCost = peopleCount * ticketCategory;
+                money = Math.Abs(moneyForTickets - ticketsCost);
             }
             else if (category == "Normal")
             {
                 ticketCategory = 249.99;
-                if (people >= 1 && people <= 4) { transport = budget * 0.75; }
-                else if (people >= 5 && people <= 9) { transport = budget * 0.60; }
-                else if (people >= 10 && people <= 24) { transport = budget * 0.50; }
-                else if (people >= 25 && people <= 49) { transport = budget * 0.40; }
-                else if (people >= 50) { transport = budget * 0.25; }
-                ticketMoney = budget - transport;
-                ticketPeople = people * ticketCategory;
-                money = Math.Abs(ticketMoney - ticketPeople);
+                if (peopleCount >= 1 && peopleCount <= 4)
+                {
+                    transportCost = budget * 0.75;
+                }
+                else if (peopleCount >= 5 && peopleCount <= 9)
+                {
+                    transportCost = budget * 0.60;
+                }
+                else if (peopleCount >= 10 && peopleCount <= 24)
+                {
+                    transportCost = budget * 0.50;
+                }
+                else if (peopleCount >= 25 && peopleCount <= 49)
+                {
+                    transportCost = budget * 0.40;
+                }
+                else if (peopleCount >= 50) 
+                {
+                    transportCost = budget * 0.25;
+                }
+                
+                moneyForTickets = budget - transportCost;
+                ticketsCost = peopleCount * ticketCategory;
+                money = Math.Abs(moneyForTickets - ticketsCost);
             }
             
-            if (ticketMoney >= ticketPeople)
+            if (moneyForTickets >= ticketsCost)
             {
                 Console.WriteLine("Yes! You have {0:f2} leva left.", money);
             }
