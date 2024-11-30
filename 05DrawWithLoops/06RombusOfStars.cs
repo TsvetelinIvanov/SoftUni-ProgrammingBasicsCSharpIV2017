1281ba18cf29765356ba0530a69d4c400eb2b0ba
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-30 17:22:44 +0200

    Update 06RombusOfStars.cs

--- 05DrawWithLoops/06RombusOfStars.cs
+++ 05DrawWithLoops/06RombusOfStars.cs
@@ -10,27 +10,26 @@ namespace _06RombusOfStars
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            
+            int n = int.Parse(Console.ReadLine());            
             for (int i = 1; i <= n; i++)
             {
-                Console.Write(new string(' ', n - i));
-                
+                Console.Write(new string(' ', n - i));                
                 for (int j = 0; j < i; j++)
                 {
                     Console.Write("* ");
                 }
+                
                 Console.WriteLine();
             }
             
             for (int i = n - 1; i >= 1; i--)
             {
                 Console.Write(new string(' ', n - i));
-
                 for (int j = 0; j < i; j++)
                 {
                     Console.Write("* ");
                 }
+                
                 Console.WriteLine();
             }
         }
