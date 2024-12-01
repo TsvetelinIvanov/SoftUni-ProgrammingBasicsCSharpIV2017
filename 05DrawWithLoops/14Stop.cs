ffbb81913b29ddc785ab952642fe88a3ffdf6689
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-01 14:08:26 +0200

    Update 14Stop.cs

--- 05DrawWithLoops/14Stop.cs
+++ 05DrawWithLoops/14Stop.cs
@@ -11,25 +11,23 @@ namespace _14Stop
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            
+	    int pointsCount = n;
+            int middleCount = 2 * n - 1;
             Console.WriteLine("{0}{1}{0}", new string('.', n + 1), new string('_', 2 * n + 1));
-            
-            int point = n;
-            int mid = 2 * n - 1;
             for (int i = 0; i < n; i++)
-			      {
-			          Console.WriteLine(@"{0}//{1}\\{0}", new string('.', point), new string('_', mid));
-                point--;
-                mid += 2;
-			      }
+	    {
+		Console.WriteLine(@"{0}//{1}\\{0}", new string('.', pointsCount), new string('_', middleCount));
+                pointsCount--;
+                middleCount += 2;
+	    }
             
-            Console.WriteLine(@"//{0}STOP!{0}\\", new string('_', mid / 2 - 2));
-            
+            Console.WriteLine(@"//{0}STOP!{0}\\", new string('_', middleCount / 2 - 2));
+		
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(@"{0}\\{1}//{0}", new string('.', point), new string('_', mid));
-                point++;
-                mid -= 2;
+                Console.WriteLine(@"{0}\\{1}//{0}", new string('.', pointsCount), new string('_', middleCount));
+                pointsCount++;
+                middleCount -= 2;
             }
         }
     }
