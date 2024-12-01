6c3533c6fc11302386d16873aafa7c9eb738495f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-01 14:16:02 +0200

    Update 15Diamond.cs

--- 05DrawWithLoops/15Diamond.cs
+++ 05DrawWithLoops/15Diamond.cs
@@ -11,22 +11,19 @@ namespace _15Diamond
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n * 3));
-            
-            int mid = n * 3;
+            int middleCount = n * 3;
+            Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n * 3));            
             for (int i = 1; i < n; i++)
             {
-                Console.WriteLine("{0}*{1}*{0}", new string('.', n - i), new string('.', mid));
-                mid += 2;
+                Console.WriteLine("{0}*{1}*{0}", new string('.', n - i), new string('.', middleCount));
+                middleCount += 2;
             }
             
             Console.WriteLine(new string('*', n * 5));
-            
             for (int i = 1; i <= n * 2; i++)
             {
-                Console.WriteLine("{0}*{1}*{0}", new string('.', i), new string('.', mid - 2));
-                mid -= 2;
+                Console.WriteLine("{0}*{1}*{0}", new string('.', i), new string('.', middleCount - 2));
+                middleCount -= 2;
             }
             
             Console.WriteLine("{0}{1}{0}", new string('.', n * 2 + 1), new string('*', n - 2));
