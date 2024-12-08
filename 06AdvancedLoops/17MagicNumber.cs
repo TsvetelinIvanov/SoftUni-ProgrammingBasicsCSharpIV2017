b7a4e8ed2856d6683b9df8d12da4e7c637aea4ad
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-08 16:18:45 +0200

    Update 17MagicNumber.cs

--- 06AdvancedLoops/17MagicNumber.cs
+++ 06AdvancedLoops/17MagicNumber.cs
@@ -11,28 +11,28 @@ namespace _17MagicNumber
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());         
-               for (int n1 = 1; n1 <= 9; n1++)
+            for (int n1 = 1; n1 <= 9; n1++)
+            {
+                for (int n2 = 1; n2 <= 9; n2++)
                 {
-                    for (int n2 = 1; n2 <= 9; n2++)
+                    for (int n3 = 1; n3 <= 9; n3++)
                     {
-                        for (int n3 = 1; n3 <= 9; n3++)
+                        for (int n4 = 1; n4 <= 9; n4++)
                         {
-                            for (int n4 = 1; n4 <= 9; n4++)
+                            for (int n5 = 1; n5 <= 9; n5++)
                             {
-                                for (int n5 = 1; n5 <= 9; n5++)
+                                for (int n6 = 1; n6 <= 9; n6++)
                                 {
-                                    for (int n6 = 1; n6 <= 9; n6++)
+                                    if (n1 * n2 * n3 * n4 * n5 * n6 == n)
                                     {
-                                        if (n1 * n2 * n3 * n4 * n5 * n6 == n)
-                                        {
-                                            Console.Write("{0}{1}{2}{3}{4}{5} ", n1, n2, n3, n4, n5, n6);
-                                        }                                        
-                                    }
+                                        Console.Write("{0}{1}{2}{3}{4}{5} ", n1, n2, n3, n4, n5, n6);
+                                    }                                        
                                 }
                             }
                         }
                     }
-                }                 
+                }
+            }                 
         }
     }
 }
