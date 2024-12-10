016aadc5d0f6ae57abff5ed62beea6a1f5b7c541
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-10 12:47:14 +0200

    Update 09PerfectDiamond.cs

--- 07SampleProgrammingBasicsExam/09PerfectDiamond.cs
+++ 07SampleProgrammingBasicsExam/09PerfectDiamond.cs
@@ -11,16 +11,16 @@ namespace _09PerfectDiamond
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             for (int i = 0; i < n; i++)
-			      {
+	    {
                 Console.Write(new string(' ', n - 1 - i));
                 for (int j = 0; j < i; j++)
                 {
                     Console.Write("*-");
                 }
+		    
                 Console.WriteLine("*");
-			      }            
+	    }            
 
             for (int i = n - 2; i >= 0; i--)
             {
@@ -29,6 +29,7 @@ namespace _09PerfectDiamond
                 {
                     Console.Write("*-");
                 }
+		
                 Console.WriteLine("*");
             }
         }
