4ab87196dda718615ed2e28d5f654e4056a9868c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-11 01:26:31 +0200

    Update 05Fox.cs

--- 08ExamPreparationProgrammingBasics18VI2017/05Fox.cs
+++ 08ExamPreparationProgrammingBasics18VI2017/05Fox.cs
@@ -11,29 +11,29 @@ namespace _05Fox
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int lenght = 2 * n + 3;
+            int length = 2 * n + 3;
 
             for (int i = 1; i <= n; i++)
-	          {
-                int asterics = i;
-                int dashes = lenght - 2 - 2 * asterics;
-                Console.WriteLine(@"{0}\{1}/{0}", new string('*', asterics), new string('-', dashes));
-	          }
+	    {
+                int asterisksCount = i;
+                int dashesCount = length - 2 - 2 * asterisksCount;
+                Console.WriteLine(@"{0}\{1}/{0}", new string('*', asterisksCount), new string('-', dashesCount));
+	    }
 
-            int midleAsterics = n;
-            int sideAsterics = (lenght - midleAsterics - 4) / 2;
+            int middleAsterisksCount = n;
+            int sideAsterisksCount = (lenght - midleAsteriskCount - 4) / 2;
             for (int i = 0; i < n / 3; i++)
-	          {                
-                Console.WriteLine(@"|{0}\{1}/{0}|", new string('*', sideAsterics), new string('*', midleAsterics));
+	    {                
+                Console.WriteLine(@"|{0}\{1}/{0}|", new string('*', sideAsterisksCount), new string('*', midleAsterisksCount));
                 sideAsterics++;
                 midleAsterics -= 2;
-	          }
+	    }
 
             for (int i = 1; i <= n; i++)
             {
                 int asterics = i;
-                int dashes = lenght - 2 - 2 * asterics;
-                Console.WriteLine(@"{0}\{1}/{0}", new string('-', asterics), new string('*', dashes));
+                int dashesCount = lenght - 2 - 2 * asterisksCount;
+                Console.WriteLine(@"{0}\{1}/{0}", new string('-', asterisksCount), new string('*', dashesCount));
             }
         }
     }
