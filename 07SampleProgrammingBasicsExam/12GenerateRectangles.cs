b563bcdbc3178d56050adb6075290ab6db3b6c9d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-10 13:08:19 +0200

    Update 12GenerateRectangles.cs

--- 07SampleProgrammingBasicsExam/12GenerateRectangles.cs
+++ 07SampleProgrammingBasicsExam/12GenerateRectangles.cs
@@ -13,7 +13,6 @@ namespace _12GenerateRectangles
             int n = int.Parse(Console.ReadLine());
             int m = int.Parse(Console.ReadLine());
             bool found = false;
-
             for (int i1 = - n; i1 <= 0; i1++)
             {
                 for (int i2 = n; i2 >= 0; i2--)
@@ -24,13 +23,12 @@ namespace _12GenerateRectangles
                         {
                             if (- n <= i1 && i1 < i2 && i2 <= n && - n <= i3 && i3 < i4 && i4 <= n)
                             { 
-                            int side1 = Math.Abs(i1) + Math.Abs(i2);
-                            int side2 = Math.Abs(i3) + Math.Abs(i4);
-                            
-                               if (side1 * side2 >= m)
-                               {
-                                   Console.WriteLine("({0}, {1}) ({2}, {3}) -> {4}", i1, i3, i2, i4, side1 * side2);
-                                found = true;
+                                int side1 = Math.Abs(i1) + Math.Abs(i2);
+                                int side2 = Math.Abs(i3) + Math.Abs(i4);                            
+                                if (side1 * side2 >= m)
+                                {
+                                    Console.WriteLine("({0}, {1}) ({2}, {3}) -> {4}", i1, i3, i2, i4, side1 * side2);
+                                    found = true;
                                 }
                             }
                         }
@@ -38,7 +36,7 @@ namespace _12GenerateRectangles
                 }
             }
 
-            if (! found)
+            if (!found)
             {
                 Console.WriteLine("No");
             }
