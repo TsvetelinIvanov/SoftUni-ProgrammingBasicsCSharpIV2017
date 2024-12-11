e5656d8a7f168995c7370663428658f05b22be1d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-11 19:50:08 +0200

    Update 05SoftUniLogo.cs

--- ExamProgrammingBasics18III2017/05SoftUniLogo.cs
+++ ExamProgrammingBasics18III2017/05SoftUniLogo.cs
@@ -11,29 +11,29 @@ namespace _05SoftUniLogo
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            
             int width = 12 * n - 5;
             int height = 4 * n - 2;
-            int mid = 0;
-
+            int middle = 0;
             for (int i = 0; i < 2 * n; i++)
             {
-                Console.WriteLine("{0}{1}#{1}{0}", new string('.', ((width - 1) / 2) - mid), new string('#', mid));
-                mid += 3;
+                Console.WriteLine("{0}{1}#{1}{0}", new string('.', ((width - 1) / 2) - middle), new string('#', middle));
+                middle += 3;
             }
             
-            mid -= 6;
+            middle -= 6;
             for (int i = 0; i < n - 2; i++)
             {
-                Console.WriteLine("|{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - mid - 1), new string('#', mid));
-                mid -= 3;
+                Console.WriteLine("|{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - middle - 1), new string('#', middle));
+                middle -= 3;
             }
             
             for (int i = 0; i < n - 1; i++)
             {
-                Console.WriteLine("|{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - mid - 1), new string('#', mid));                
+                Console.WriteLine("|{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - middle - 1), new string('#', middle));                
             }
             
-            Console.WriteLine("@{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - mid - 1), new string('#', mid));
+            Console.WriteLine("@{0}{1}#{1}{0}.", new string('.', ((width - 1) / 2) - middle - 1), new string('#', middle));
         }
     }
 }
