936d2f1f6127452daa57873d3af3d11c8de67953
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-14 12:29:50 +0200

    Update 03Flowers.cs

--- ExamProgrammingBasics18XII2016/03Flowers.cs
+++ ExamProgrammingBasics18XII2016/03Flowers.cs
@@ -15,30 +15,49 @@ namespace _03Flowers
             int tulipQuontity = int.Parse(Console.ReadLine());
             string season = Console.ReadLine();
             string day = Console.ReadLine();
+            
             double chrysanthemumPrice = 0.0;
             double rosePrice = 0.0;
             double tulipPrice = 0.0;
-
             switch (season)
             {
                 case "Spring":
-                case "Summer": chrysanthemumPrice = 2.0; rosePrice = 4.1; tulipPrice = 2.5; break;
+                case "Summer":
+                    chrysanthemumPrice = 2.0;
+                    rosePrice = 4.1;
+                    tulipPrice = 2.5;
+                    break;
                 case "Autumn":
-                case "Winter": chrysanthemumPrice = 3.75; rosePrice = 4.5; tulipPrice = 4.15; break;
+                case "Winter":
+                    chrysanthemumPrice = 3.75;
+                    rosePrice = 4.5;
+                    tulipPrice = 4.15;
+                    break;
             }
 
-            double flowerPrice = chrysanthemumPrice * chrysanthemumQuontity + rosePrice * roseQuontity + tulipPrice * tulipQuontity;
+            double flowersPrice = chrysanthemumPrice * chrysanthemumQuontity + rosePrice * roseQuontity + tulipPrice * tulipQuontity;
+            if (day == "Y")
+            {
+                flowersPrice += flowersPrice * 0.15;
+            }
 
-            if (day == "Y") flowerPrice += flowerPrice * 0.15;
+            if (tulipQuontity > 7 && season == "Spring")
+            {
+                flowersPrice -= flowersPrice * 0.05;
+            }
 
-            if (tulipQuontity > 7 && season == "Spring") flowerPrice -= flowerPrice * 0.05;
+            if (roseQuontity >= 10 && season == "Winter")
+            {
+                flowersPrice -= flowersPrice * 0.1;
+            }
 
-            if (roseQuontity >= 10 && season == "Winter") flowerPrice -= flowerPrice * 0.1;
+            if ((chrysanthemumQuontity + roseQuontity + tulipQuontity) > 20)
+            {
+                flowersPrice -= flowersPrice * 0.2;
+            }
 
-            if ((chrysanthemumQuontity + roseQuontity + tulipQuontity) > 20) flowerPrice -= flowerPrice * 0.2;
-
-            flowerPrice += 2;
-            Console.WriteLine("{0:f2}", flowerPrice);
+            flowersPrice += 2;
+            Console.WriteLine("{0:f2}", flowersPrice);
         }
     }
 }
