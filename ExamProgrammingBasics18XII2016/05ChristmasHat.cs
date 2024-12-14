e68aa172f32046a0a1b2aea86225228f1ac9c7e2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-14 13:04:12 +0200

    Update 05ChristmasHat.cs

--- ExamProgrammingBasics18XII2016/05ChristmasHat.cs
+++ ExamProgrammingBasics18XII2016/05ChristmasHat.cs
@@ -14,20 +14,18 @@ namespace _05ChristmasHat
             int width = 4 * n + 1;
 
             Console.WriteLine(@"{0}/|\{0}", new string('.', 2 * n - 1));
-
             Console.WriteLine(@"{0}\|/{0}", new string('.', 2 * n - 1));
-
             for (int i = 0; i < 2 * n; i++)
             {
                 Console.WriteLine("{0}*{1}*{1}*{0}", new string('.', 2 * n - 1 - i), new string('-', i));
             }
 
             Console.WriteLine(new string('*', width));
-
             for (int i = 0; i < n * 2; i++)
             {
                 Console.Write("*.");
             }
+            
             Console.WriteLine("*");
 
             Console.WriteLine(new string('*', width));
