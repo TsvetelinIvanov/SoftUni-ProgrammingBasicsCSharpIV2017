0c2ac351882dec95786e4c928c4ca8075f391504
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-15 22:05:12 +0200

    Update 03FruitCoctails.cs

--- ExamProgrammingBasics25VI2017/03FruitCoctails.cs
+++ ExamProgrammingBasics25VI2017/03FruitCoctails.cs
@@ -13,46 +13,66 @@ namespace _03FruitCoctails
             string fruit = Console.ReadLine();
             string volume = Console.ReadLine();
             int number = int.Parse(Console.ReadLine());
+            
             double price = 0.0;
             double cost = 0.0;
-
             switch (fruit)
             {
                 case "Watermelon":
                     {
                         switch (volume)
                         {
-                            case "small": price = 2.0 * 56; break;
-                            case "big": price = 5.0 * 28.7; break;
+                            case "small":
+                                price = 2.0 * 56;
+                                break;
+                            case "big":
+                                price = 5.0 * 28.7;
+                                break;
                         }
                     }
+                    
                         break;
                 case "Mango":
                         {
                             switch (volume)
                             {
-                                case "small": price = 2.0 * 36.66; break;
-                                case "big": price = 5.0 * 19.6; break;
+                                case "small":
+                                    price = 2.0 * 36.66;
+                                    break;
+                                case "big":
+                                    price = 5.0 * 19.6;
+                                    break;
                             }
                         }
+                    
                         break;
                 case "Pineapple":
                         {
                             switch (volume)
                             {
-                                case "small": price = 2.0 * 42.1; break;
-                                case "big": price = 5.0 * 24.8; break;
+                                case "small":
+                                    price = 2.0 * 42.1;
+                                    break;
+                                case "big":
+                                    price = 5.0 * 24.8;
+                                    break;
                             }
                         }
+                    
                         break;
                 case "Raspberry":
                         {
                             switch (volume)
                             {
-                                case "small": price = 2.0 * 20; break;
-                                case "big": price = 5.0 * 15.2; break;
+                                case "small":
+                                    price = 2.0 * 20;
+                                    break;
+                                case "big":
+                                    price = 5.0 * 15.2;
+                                    break;
                             }
                         }
+                    
                         break;
             }
 
