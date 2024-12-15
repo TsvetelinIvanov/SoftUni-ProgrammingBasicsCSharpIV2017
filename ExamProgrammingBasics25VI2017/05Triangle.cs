c29ebda4a67f4b2e32b6b141e4cc057ad2ecc00c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-15 22:19:18 +0200

    Update 05Triangle.cs

--- ExamProgrammingBasics25VI2017/05Triangle.cs
+++ ExamProgrammingBasics25VI2017/05Triangle.cs
@@ -11,30 +11,29 @@ namespace _05Triangle
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int width = (4 * n) + 1;
-            int height = (2 * n) + 1 ;
-            int mid = 1;
+            int width = (4 * n) + 1;            
+            int middle = 1;
 
             Console.WriteLine(new string('#', width));
             for (int i = 1; i <= n; i++)
             {              
-                    if (i == (n / 2) + 1)
-                    {
-                        Console.WriteLine(@"{0}{1}{2}(@){2}{1}{0}", new string('.', i), new string('#', 2 * n - i - (mid / 2)), new string(' ', mid / 2 - 1));
-                        mid += 2;
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0}{1}{2}{1}{0}", new string('.', i), new string('#', 2 * n - i - (mid / 2)), new string(' ', mid));
-                        mid += 2;
-                    }                    
+                if (i == (n / 2) + 1)
+                {
+                    Console.WriteLine(@"{0}{1}{2}(@){2}{1}{0}", new string('.', i), new string('#', 2 * n - i - (middle / 2)), new string(' ', middle / 2 - 1));
+                    middle += 2;
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{1}{0}", new string('.', i), new string('#', 2 * n - i - (middle / 2)), new string(' ', middle));
+                    middle += 2;
+                }                    
             }                 
             
-            mid -= 2; 
+            middle -= 2; 
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("{0}{1}{0}", new string('.', (width / 2) - (mid / 2)), new string('#', mid));
-                mid -= 2;
+                Console.WriteLine("{0}{1}{0}", new string('.', (width / 2) - (middle / 2)), new string('#', middle));
+                middle -= 2;
             }
         }        
     }
