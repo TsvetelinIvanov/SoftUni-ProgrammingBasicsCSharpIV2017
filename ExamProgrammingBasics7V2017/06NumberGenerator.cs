974ea16b5a723233f7cc8c624627cd89ea43de83
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-16 12:24:28 +0200

    Update 06NumberGenerator.cs

--- ExamProgrammingBasics7V2017/06NumberGenerator.cs
+++ ExamProgrammingBasics7V2017/06NumberGenerator.cs
@@ -13,9 +13,9 @@ namespace _06NumberGenerator
             int M = int.Parse(Console.ReadLine());
             int N = int.Parse(Console.ReadLine());
             int L = int.Parse(Console.ReadLine());
-            int spetialNumber = int.Parse(Console.ReadLine());
+            int specialNumber = int.Parse(Console.ReadLine());
             int controlNumber = int.Parse(Console.ReadLine());
-           
+            
             for (int m = M; m >= 1; m--)
             {
                 for (int n = N; n >= 1; n--)
@@ -25,30 +25,41 @@ namespace _06NumberGenerator
                         int mnl = (m * 100) + (n * 10) + l;
                         if (mnl % 3 == 0)
                         {
-                            spetialNumber += 5;
+                            specialNumber += 5;
                         }
                         else if (mnl % 10 == 5)
                         {
-                            spetialNumber -= 2;
+                            specialNumber -= 2;
                         }
                         else if (mnl % 2 == 0)
                         {
-                            spetialNumber *= 2;
+                            specialNumber *= 2;
                         }
-                        if (spetialNumber >= controlNumber) break;
+                        
+                        if (specialNumber >= controlNumber)
+                        {
+                            break;
+                        }
                     }
-                    if (spetialNumber >= controlNumber) break;
+                    
+                    if (specialNumber >= controlNumber)
+                    {
+                        break;
+                    }
                 }
-                if (spetialNumber >= controlNumber) break;
+                if (specialNumber >= controlNumber)
+                {
+                    break;
+                }
             }
 
-            if (spetialNumber >= controlNumber)
+            if (specialNumber >= controlNumber)
             {
-                Console.WriteLine("Yes! Control number was reached! Current special number is {0}.", spetialNumber);
+                Console.WriteLine("Yes! Control number was reached! Current special number is {0}.", specialNumber);
             }
             else
             {
-                Console.WriteLine("No! {0} is the last reached special number.", spetialNumber);
+                Console.WriteLine("No! {0} is the last reached special number.", specialNumber);
             }
         }
     }
