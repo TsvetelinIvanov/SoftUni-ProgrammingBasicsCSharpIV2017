51472e91e5bdeaaebf4c637bc046fd0b6bd1a753
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-16 12:15:59 +0200

    Update 05Crown.cs

--- ExamProgrammingBasics7V2017/05Crown.cs
+++ ExamProgrammingBasics7V2017/05Crown.cs
@@ -15,17 +15,17 @@ namespace _05Crown
             Console.WriteLine("@{0}@{0}@", new string(' ', n - 2));
             Console.WriteLine("**{0}*{0}**", new string(' ', n - 3));
 
-            int mid = 1;
+            int middle = 1;
             int air = n - 5;
             for (int i = 1; i <= (n / 2) - 2; i++)
             {
-                Console.WriteLine("*{0}*{1}*{2}*{1}*{0}*", new string('.', i), new string(' ', air), new string('.', mid));
+                Console.WriteLine("*{0}*{1}*{2}*{1}*{0}*", new string('.', i), new string(' ', air), new string('.', middle));
                 air -= 2;
-                mid += 2;
+                middle += 2;
             }
 
-            Console.WriteLine("*{0}*{1}*{0}*", new string('.', (n / 2) - 1), new string('.', mid));
-            Console.WriteLine("*{0}{1}.{1}{0}*", new string('.', n / 2), new string('*', mid / 2));
+            Console.WriteLine("*{0}*{1}*{0}*", new string('.', (n / 2) - 1), new string('.', middle));
+            Console.WriteLine("*{0}{1}.{1}{0}*", new string('.', n / 2), new string('*', middle / 2));
             Console.WriteLine(new string('*', 2 * n - 1));
             Console.WriteLine(new string('*', 2 * n - 1));
         }
