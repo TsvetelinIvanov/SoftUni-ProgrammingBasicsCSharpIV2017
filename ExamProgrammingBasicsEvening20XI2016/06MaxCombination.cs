048a81d5d2376c85054678b4bca0e2a9d5d748ee
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-19 00:06:04 +0200

    Update 06MaxCombination.cs

--- ExamProgrammingBasicsEvening20XI2016/06MaxCombination.cs
+++ ExamProgrammingBasicsEvening20XI2016/06MaxCombination.cs
@@ -13,17 +13,23 @@ namespace _06MaxCombination
             int start = int.Parse(Console.ReadLine());
             int end = int.Parse(Console.ReadLine());
             int maxCombination = int.Parse(Console.ReadLine());
+            
             int counter = 0;
-
             for (int i = start; i <= end; i++)
             {
                 for (int j = start; j <= end; j++)
                 {
                     Console.Write("<{0}-{1}>", i, j);
                     counter++;
-                    if (counter >= maxCombination) break;
+                    if (counter >= maxCombination)
+                    {
+                        break;
+                    }
                 }
-                if (counter >= maxCombination) break;
+                if (counter >= maxCombination)
+                {
+                    break;
+                }
             }
         }
     }
