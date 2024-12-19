5883e809e10a2f4ad498e9edad0a5776064d6767
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-19 20:05:40 +0200

    Update 05Hourglass.cs

--- ExamProgrammingBasicsMorning19III2017/05Hourglass.cs
+++ ExamProgrammingBasicsMorning19III2017/05Hourglass.cs
@@ -11,19 +11,21 @@ namespace _05Hourglass
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             int width = 2 * n + 1;
+            
             Console.WriteLine(new string('*', width));
             Console.WriteLine(".*{0}*.", new string(' ', width - 4));
             for (int i = 2; i < n; i++)
             {
                 Console.WriteLine("{0}*{1}*{0}", new string('.', i), new string('@', width - 2 - 2 * i));
             }
+            
             Console.WriteLine("{0}*{0}", new string('.', n));
             for (int i = 0; i < n -2; i++)
             {
                 Console.WriteLine("{0}*{1}@{1}*{0}", new string('.', n - 1 - i), new string(' ', i));
             }
+            
             Console.WriteLine(".*{0}*.", new string('@', width - 4));
             Console.WriteLine(new string('*', width));
         }
