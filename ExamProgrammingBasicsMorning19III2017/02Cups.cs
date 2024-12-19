0320bf88c88aba9e523ba0255811e5a5825f0ef2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-19 19:42:32 +0200

    Update 02Cups.cs

--- ExamProgrammingBasicsMorning19III2017/02Cups.cs
+++ ExamProgrammingBasicsMorning19III2017/02Cups.cs
@@ -10,22 +10,21 @@ namespace _02Cups
     {
         static void Main(string[] args)
         {
-            int glassesExpected = int.Parse(Console.ReadLine());
-            int workers = int.Parse(Console.ReadLine());
-            int workDays = int.Parse(Console.ReadLine());
+            int expectedGlassesCount = int.Parse(Console.ReadLine());
+            int workersCount = int.Parse(Console.ReadLine());
+            int workDaysCount = int.Parse(Console.ReadLine());
 
-            decimal allHours = (decimal)workers * workDays * 8m;
-            decimal glasses = Math.Floor(allHours / 5);
-            decimal diff = Math.Abs(glasses - glassesExpected);
-            decimal sum = diff * 4.20m;
-
-            if (glasses < glassesExpected)
+            decimal allHoursCount = (decimal)workersCount * workDaysCount * 8m;
+            decimal glassesCount = Math.Floor(allHoursCount / 5);
+            decimal difference = Math.Abs(glassesCount - expectedGlassesCount);
+            decimal money = difference * 4.20m;
+            if (glassesCount < expectedGlassesCount)
             {
-                Console.WriteLine("Losses: {0:f2}", sum);
+                Console.WriteLine("Losses: {0:f2}", money);
             }
             else
             {
-                Console.WriteLine("{0:f2} extra money", sum);
+                Console.WriteLine("{0:f2} extra money", money);
             }
         }
     }
