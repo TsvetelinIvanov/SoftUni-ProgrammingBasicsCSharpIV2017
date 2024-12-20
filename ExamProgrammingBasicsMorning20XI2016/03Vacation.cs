c7ef402e56f39ac036e3b937f7696f9ab3d6bee0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-20 11:46:42 +0200

    Update 03Vacation.cs

--- ExamProgrammingBasicsMorning20XI2016/03Vacation.cs
+++ ExamProgrammingBasicsMorning20XI2016/03Vacation.cs
@@ -10,29 +10,41 @@ namespace _03Vacation
     {
         static void Main(string[] args)
         {
-            int adults = int.Parse(Console.ReadLine());
-            int kids = int.Parse(Console.ReadLine());
-            int nights = int.Parse(Console.ReadLine());
+            int adultsCount = int.Parse(Console.ReadLine());
+            int kidsCount = int.Parse(Console.ReadLine());
+            int nightsCount = int.Parse(Console.ReadLine());
             string transportKind = Console.ReadLine();
+            
             double adultsTransportCost = 0.0;
             double kidsTransportCost = 0.0;
-
             switch (transportKind)
             {
-                case "train": adultsTransportCost = 24.99; kidsTransportCost = 14.99; 
-                    if (adults + kids >= 50)
+                case "train":
+                    adultsTransportCost = 24.99;
+                    kidsTransportCost = 14.99; 
+                    if (adultsCount + kidsCount >= 50)
                     {
                         adultsTransportCost *= 0.5;
                         kidsTransportCost *= 0.5;
                     }
+                    
                     break;
-                case "bus": adultsTransportCost = 32.50; kidsTransportCost = 28.50; break;
-                case "boat": adultsTransportCost = 42.99; kidsTransportCost = 39.99; break;
-                case "airplane": adultsTransportCost = 70.0; kidsTransportCost = 50.0; break;
+                case "bus":
+                    adultsTransportCost = 32.50;
+                    kidsTransportCost = 28.50;
+                    break;
+                case "boat":
+                    adultsTransportCost = 42.99;
+                    kidsTransportCost = 39.99;
+                    break;
+                case "airplane":
+                    adultsTransportCost = 70.0;
+                    kidsTransportCost = 50.0;
+                    break;
             }
 
-            double transportCost = (adultsTransportCost * adults + kidsTransportCost * kids) * 2;
-            double hotelCost = 82.99 * nights;
+            double transportCost = (adultsTransportCost * adultsCount + kidsTransportCost * kidsCount) * 2;
+            double hotelCost = 82.99 * nightsCount;
             double commission = (transportCost + hotelCost) / 10.0;
             double totalCost = transportCost + hotelCost + commission;
 
